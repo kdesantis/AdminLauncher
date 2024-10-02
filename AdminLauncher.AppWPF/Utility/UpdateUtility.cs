a271df4d28b82192e2bfde7a139bdebd007519ad
Kdesantis <Kevindesantis95@gmail.com>
2024-10-02 09:20:32 +0200

    Corretti button nelle message box di aggiornamento

--- AdminLauncher.AppWPF/Utility/UpdateUtility.cs
+++ AdminLauncher.AppWPF/Utility/UpdateUtility.cs
@@ -21,13 +21,13 @@ namespace AdminLauncher.AppWPF.Utility
             else if (updater.Error != null)
             {
                 var message = $"Error in searching for updates";
-                MessageBox.Show(message, "Update Error", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                MessageBox.Show(message, "Update Error", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
         private static void NotifyUserForUpdate(ReleaseInformation update)
         {
             var message = $"A new version ({update.Version}) is available. Do you want to download it?\n\nRelease note:\n{update.ReleaseNotes}";
-            var result = MessageBox.Show(message, "Error in searching for updates", MessageBoxButton.OK, MessageBoxImage.Information);
+            var result = MessageBox.Show(message, "Error in searching for updates", MessageBoxButton.YesNo, MessageBoxImage.Information);
 
             if (result == MessageBoxResult.Yes)
             {
