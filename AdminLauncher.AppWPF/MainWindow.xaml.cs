6a97273b6220f1063b4304cd8534686876a8272e
Kevin De Santis <kevindesantis95@gmail.com>
2024-11-25 17:09:11 +0100

    Fix wizard window dimension for low resolutions monitor

--- AdminLauncher.AppWPF/MainWindow.xaml.cs
+++ AdminLauncher.AppWPF/MainWindow.xaml.cs
@@ -285,7 +285,13 @@ namespace AdminLauncher.AppWPF
             double mainLeft = this.Left;
             double mainTop = this.Top;
             double mainWidth = this.Width;
-            wizardWindow.Left = mainLeft - wizardWindow.Width; ;
+
+            double workAreaWidth = SystemParameters.WorkArea.Width;
+
+            wizardWindow.Left = mainLeft - wizardWindow.Width;
+            if (workAreaWidth < mainWidth + wizardWindow.Width)
+                wizardWindow.Left = 0;
+
             wizardWindow.Top = mainTop;
             var result = wizardWindow.ShowDialog();
             if (result == true)
